b34c0ef57d56fe2e52d59f1287e884b1a883d672
altabored <alegutier.021@gmail.com>
2023-11-11 21:11:53 -0600

    feat: Implementación clase ERRORES y ANALIZADOR, distribución de la lectura del código

--- Principal.py
+++ Principal.py
@@ -1,26 +1,14 @@
 from io import open
+from analizador import *
 
-linea = 1
+nroLinea = 1
+analizador = Analizador()
 
-# Por algún motivo sin el 'encoding' no lee el archivo. Ahí se quedará.
-#
-# El código de abajo es una prueba para verificar que el programa rescate bien las lineas del TXT,
-# y que permita trabajar bien con sus respectivos valores
+# Por algún motivo sin el 'encoding' no lee el archivo.
+with open("codigo.txt", 'r', encoding="utf8") as archivo: 
 
-with open("codigo.txt", 'r', encoding="utf8") as file:
+    for linea in archivo.readlines(): # Del archivo .txt, lee una line
+        
+        analizador.procesar_linea(nroLinea, linea)
 
-    print("\n")
-    for line in file.readlines(): # Del archivo .txt, lee una linea
-        print("+++++[ Linea #{} de código ]+++++\n".format(linea))
-        print(line)
-
-        print("\n*****[ Palabras de la linea #{} ]*****\n".format(linea))
-        for word in line.split(): # De esta linea, lee cada una de sus palabras
-            print(word)
-        print("\n======================================\n")
-
-        # Todos estos print son solo para ver que sirvan bien las partes del método, al final se quitarán
-
-        # En base a esas palabras reescatadas, es que deberíamos de realizar la construcción del Hashtable
-
-        linea = linea + 1
+        nroLinea = nroLinea + 1

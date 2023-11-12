93d54a4e055724f7e7544b03fbb54cd43b9cd32f
altabored <alegutier.021@gmail.com>
2023-11-12 12:23:06 -0600

    feat: Código de prueba para procesar las declaraciones de variables

--- analizador.py
+++ analizador.py
@@ -4,23 +4,26 @@ class Analizador:
 
     def __init__(self):
         # Estos son los digitos y palabras claves que el programa aceptara al momento de leer el "código fuente"
-        tipos = ('void', 'int', 'float', 'string')
-        condicionales = ('if' ,'while')
-        operaciones = ('=', '+', '-', '<', '>')
-        puntuales = (',', '.', '[', ']', '{', '}', '(', ')')
+        self.tipos = ('void', 'int', 'float', 'string')
+        self.condicionales = ('if' ,'while')
+        
+        self.operaciones = ('+', '-', '<', '>')
+        self.puntuales = (',', '.')
+        self.simbolos_abrir = ('[', '{', '(')
+        self.simbolos_cerrar = (']', '}', ')')
 
         # Diccionario para almacenar las variables del "código fuente"
-        variables = {}
+        self.variables = {}
 
         # Lista para almacenar las variables desconocidas que se encuentren
-        desconocidos = []
+        self.desconocidos = []
 
     # Los PRINT() de abajo tienen que ser borrados, ahorita los deje para que se entienda la idea.
 
     def procesar_linea(self, numLinea, linea):
-        print("Linea {}: {} \n - Palabras:".format(numLinea, linea))
         for palabra in linea.split(): # De esta linea, lee cada una de sus palabras
             self.procesar_palabra(palabra)
         
     def procesar_palabra(self, palabra):
-        print(palabra)
+        if palabra in self.tipos:
+            print(palabra)
